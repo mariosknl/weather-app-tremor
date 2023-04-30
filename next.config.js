dc76eb71dd6932f254e9c1bd660f3f74d4d2639f
Marios Kanellopoulos <mariosknl@outlook.com>
2023-04-30 07:50:38 +0300

    creating CityPicker component

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+	experimental: {
+		appDir: true,
+		serverComponentsExternalPackages: ["@tremor/react"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
