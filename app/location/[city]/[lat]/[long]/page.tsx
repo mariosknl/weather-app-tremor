1bf08a80277b0b268076bd15de0757fd0d949ace
Marios Kanellopoulos <mariosknl@outlook.com>
2023-04-30 08:56:32 +0300

    building callout cards in the weather page && stepzen implementation for graphql

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,7 @@
+import { getClient } from "@/apollo-client";
+import CalloutCard from "@/components/CalloutCard";
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+
 type Props = {
 	params: {
 		city: string;
@@ -6,10 +10,41 @@ type Props = {
 	};
 };
 
-function WeatherPage({ params: { city, lat, long } }: Props) {
+async function WeatherPage({ params: { city, lat, long } }: Props) {
+	const client = getClient();
+
+	const { data } = await client.query({
+		query: fetchWeatherQuery,
+		variables: {
+			current_weather: "true",
+			longitude: long,
+			latitude: lat,
+			timezone: "GMT",
+		},
+	});
+
+	const results: Root = data.myQuery;
+
 	return (
 		<div>
-			Welcome to the WeatherPage: {city} {lat} {long}
+			{/* <InformationPanel /> */}
+
+			<div>
+				<div className="pb-5">
+					<div className="pb-5">
+						<h2 className="text-xl font-bold">Todays Overview</h2>
+						<p className="text-sm text-gray-400">
+							Last Update at:{" "}
+							{new Date(results.current_weather.time).toLocaleString()} (
+							{results.timezone})
+						</p>
+					</div>
+
+					<div>
+						<CalloutCard message="This is where GPT-4 Summary will go" />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
